bfd8cb26a32e05bfa25d39aa8eddb882ab7691b5
lin jiang <linjiang@micronet.ltd>
2022-10-13 13:24:09 +0800

    clear

--- bin/index.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env node
-
-const vorpal = require('vorpal')();
-vorpal.use(require('../src/cli.js'));
-// vorpal.use(require('../src/testcli.js'));
--- src/server.js
+++
@@ -1,4 +0,0 @@
-const Blockchain = require('./blockchain.js')
-
-const blockchain = new Blockchain()
-console.log('init', blockchain)
